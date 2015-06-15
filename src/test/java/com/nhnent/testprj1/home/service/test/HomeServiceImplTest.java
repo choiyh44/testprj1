f8f1c229172cfdf4d6e59e16fb31b57a910633ae
최영환[네무소프트] <choiyh44@nemosoft.com.kr>
2015-06-15 14:03:49 +0900

    klocwork 사용을 위한 설정변경

---
+++ src/test/java/com/nhnent/testprj1/home/service/test/HomeServiceImplTest.java
@@ -0,0 +1,14 @@
+package com.nhnent.testprj1.home.service.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HomeServiceImplTest {
+
+	@Test
+	public void testGetFormattedServerDate() {
+//		alertNotNull();
+	}
+
+}
